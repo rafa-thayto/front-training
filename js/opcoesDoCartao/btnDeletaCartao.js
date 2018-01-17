1adba63f717c0bb7b097581ac0f1b21fcc5814d6
Rafael Thayto <rafa.thayto@gmail.com>
2018-01-17 17:16:49 -0200

    Refactoring the code

--- js/opcoesDoCartao/btnDeletaCartao.js
+++ js/opcoesDoCartao/btnDeletaCartao.js
@@ -1,22 +1,27 @@
-// Pega todos os elementos com a class 'opcoesDoCartao-remove'
-const btnDelete = document.querySelectorAll('.opcoesDoCartao-remove');
+// (IIFE)
+(function() {
 
-// Adiciona o evento click em todos os btn
-for(let i = 0; btnDelete.length; i++) {
-
-  btnDelete[i].addEventListener('click', deleteCartao);
-}
-
-function deleteCartao() {
-  const cartao = this.parentNode.parentNode;
-
-  cartao.classList.add('cartao--some');
+  // Pega todos os elementos com a class 'opcoesDoCartao-remove'
+  const btn = document.querySelectorAll('.opcoesDoCartao-remove');
   
-  cartao.addEventListener('transitionend', function() {
-    cartao.remove();
-  });
+  // Adiciona o evento click em todos os btn
+  for(let i = 0; btn.length; i++) {
+    btn[i].addEventListener('click', function() {
+      const cartao = this.parentNode.parentNode;
+      
+      cartao.classList.add('cartao--some');
+      
+      cartao.addEventListener('transitionend', function() {
+        cartao.remove();
+      });
+      
+      // setTimeout(function() {
+        //   cartao.remove();
+        // }, 300);
+    });
+  }
   
-  // setTimeout(function() {
-  //   cartao.remove();
-  // }, 250);
-}
+  
+
+})()
+    
